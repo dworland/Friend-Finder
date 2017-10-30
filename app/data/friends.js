783f16667ea1b9bf5c4e1644e62b06d041e69a1d
Devon Worland <devonworland@Devons-MacBook-Pro.local>
2017-10-30 15:52:19 -0500

    adding friends data

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: "Rachel",
+		photo: "https://images.vogue.it/gallery/23475/Big/a4a6021a-4a91-4fd4-a658-f0f7d1c82457.jpg",
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Chandler",
+		photo: "https://i.pinimg.com/736x/8e/f2/a7/8ef2a70a6992b5f39ddf84607a38e1d7--friends-season-friends-tv-show.jpg",
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Joey",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest?cb=20070426103739",
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: "Monica",
+		photo: "https://i.pinimg.com/originals/1f/79/3d/1f793daab3f1271de4ed15749362df08.jpg",
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: "Ross",
+		photo: "https://www.thesun.co.uk/wp-content/uploads/2017/08/nintchdbpict000003441959.jpg?strip=all&w=771",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: "Phoebe",
+		photo: "https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Janice",
+		photo: "https://pbs.twimg.com/profile_images/534578109417140224/PZZETnJX_400x400.jpeg",
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: "Gunther",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4bd4IdUV_VQ-lrnbI2uyMZMetC1GHtFiMpfRtUhg1OzzzUoWF",
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Richard",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/1/13/TOMSELLECK.jpg/revision/latest?cb=20080803014323",
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: "Emily",
+		photo: "https://images2.fanpop.com/image/polls/330000/330096_1258716137788_full.jpg?v=1258716175",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
+
